a9ef5eaceb47798a46dd12221cd85c4196dc2a7a
Stephen Lee <srnlee771@gmail.com>
2019-03-12 00:02:30 -0700

    remove magic numbers and change variable access

--- BirthdayParadox.java
+++ BirthdayParadox.java
@@ -7,24 +7,25 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class BirthdayParadox {
-    public static int peopleMax = 70;
-    public static int trials = 10000;
-    public static int success = 0;
-    public static ArrayList<Integer> birthdates = new ArrayList<Integer>();
+    private static final int PEOPLE_MAX = 70;
+    private static final int TRIALS = 10000;
+    private static final double PERCENT_CONVERT = 100;
 
     public static void main(String[] args) {
+        int success = 0;
         Random randomGenerator = new Random();
-        for (int counter = 0; counter < trials; counter++){
-            for (int people = 0; people < peopleMax; people++){
+        ArrayList<Integer> birthdates = new ArrayList<Integer>();
+        for (int counter = 0; counter < TRIALS; counter++) {
+            for (int people = 0; people < PEOPLE_MAX; people++) {
                 birthdates.add(randomGenerator.nextInt(365));
             }
             Set birthdate = new HashSet<>(birthdates);
-            if (birthdate.size() < birthdates.size()){
+            if (birthdate.size() < birthdates.size()) {
                 success += 1;
             }
             birthdates.clear();
             birthdate.clear();
         }
-        System.out.println((float)(success) / (float)(trials) * 100 + "%");
+        System.out.println((float) (success) / (float) (TRIALS) * PERCENT_CONVERT + "%");
     }
 }
